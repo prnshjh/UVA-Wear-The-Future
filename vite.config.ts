3afe6856b44dcd17249915b06d818c1732913547
jhapriyanshu19 <prnshjh@gmail.com>
2025-10-18 20:13:31 +0530

    vercel:

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+
+export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    emptyOutDir: true,
+  },
+  server: {
+    port: 5173, // optional for local dev
+    open: true,
+  },
+});
